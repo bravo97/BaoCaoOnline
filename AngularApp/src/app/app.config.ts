d7d69e9fa0a7e63cc1feb4274db7ca27339ad0d8
bravo97 <dobahung941997@gmail.com>
2025-11-23 22:43:41 +0700

    Test deploy app github

--- AngularApp/src/app/app.config.ts
+++ AngularApp/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
@@ -9,6 +9,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes, withHashLocation()), // <-- hash routing đúng cách
     provideAnimations(), // required animations providers
     provideToastr(), // Toastr providers
     provideHttpClient(withInterceptors([authInterceptor])),
