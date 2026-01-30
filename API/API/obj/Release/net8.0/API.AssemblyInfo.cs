a2c924b0458fcc6bbec6c8f538315915395d8ff8
bravo97 <dobahung941997@gmail.com>
2026-01-30 09:13:49 +0700

    chỉnh sửa favicon.ico và biến môi trường enviroment

--- API/API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a484a02e60fdcad5254cae1e034d0809dd392d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41cbf0d5e121bcf67cb245f3e9e67f4b055683ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
