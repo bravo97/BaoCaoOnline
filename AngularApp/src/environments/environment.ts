c19874aad55710161b273a03e0d47baa6d244465
bravo97 <dobahung941997@gmail.com>
2026-02-02 01:41:31 +0700

    deploy angular thành công

--- AngularApp/src/environments/environment.ts
+++ AngularApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7023/api'
-    //apiUrl: 'https://baocao-ctcahqh4fuf5ahdj.canadacentral-01.azurewebsites.net/api'
+    //apiUrl: 'https://localhost:7023/api'
+    apiUrl: 'https://baocao-ctcahqh4fuf5ahdj.canadacentral-01.azurewebsites.net/api'
 };
